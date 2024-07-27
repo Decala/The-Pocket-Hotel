11134fab9a3b57a3911a3d5a8844ee8f24f558fe
Decala <declanchua15@gmail.com>
2024-07-27 02:50:08 +0000

    Added google maps

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ function get_contact(){
         document.getElementById("contact_way").placeholder = "Please enter your email"
     }
 }
+
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  var location = { lat: 1.2846, lng: 103.8528 };
+
+  map = new Map(document.getElementById("map"), {
+    center: location,
+    zoom: 20,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: location,
+    title: "The Pocket Hotel"
+  });
+}
+//Source:
+// https://developers.google.com/maps/documentation/javascript/overview
